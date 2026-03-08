cd7ddc5a299e5bb3ac21c682f0d635cec3740a25
imaaryan <imaaryanpandey@gmail.com>
2026-03-08 23:21:32 +0530

    feat: encapsulate Slick slider initialization within a function to ensure readiness and prevent re-initialization.

--- public/frontend_assets/js/slider.js
+++ public/frontend_assets/js/slider.js
@@ -1,50 +1,60 @@
-$('.testimonial-slider').slick({
-    dots: true,
-    infinite: true,
-    loop:true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: "<i class='fa fa-chevron-left'></i>",
-    nextArrow: "<i class='fa fa-chevron-right'></i>",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-            adaptiveHeight: true,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-            adaptiveHeight: true,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-});
+function initSlickSliders() {
+  if (typeof $ !== "undefined" && $.fn && $.fn.slick) {
+    if (
+      $(".testimonial-slider").length > 0 &&
+      !$(".testimonial-slider").hasClass("slick-initialized")
+    ) {
+      $(".testimonial-slider").slick({
+        dots: true,
+        infinite: true,
+        loop: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: "<i class='fa fa-chevron-left'></i>",
+        nextArrow: "<i class='fa fa-chevron-right'></i>",
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              adaptiveHeight: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              adaptiveHeight: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+    }
+  } else {
+    setTimeout(initSlickSliders, 50);
+  }
+}
+
+initSlickSliders();
 
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 200)
-    {
-      $('header').addClass('sticky');
+    if ($(this).scrollTop() > 200) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
     }
-    else
-    {
-      $('header').removeClass('sticky');
-    }
-  })
-})
+  });
+});
