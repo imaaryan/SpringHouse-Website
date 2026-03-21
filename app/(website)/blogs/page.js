7bef119d895a567c75af1cf6710fefd1afc2914d
imaaryan <imaaryanpandey@gmail.com>
2026-03-21 23:37:56 +0530

    style: Enhance blog card UI by adding decorative SVG shapes and updating card, image, content, and text styles.

--- app/(website)/blogs/page.js
+++ app/(website)/blogs/page.js
@@ -19,7 +19,7 @@ export default async function BlogsArchivePage() {
   return (
     <>
       <Header />
-      <div className="blogs blog-archive pt30 pb60" style={{  background: "#f8f9fa" }}>
+      <div className="blogs blog-archive pt30 pb60 blog" style={{ background: "#f8f9fa" }}>
         <div className="container-fluid pt-4">
           <div className="row mb-5">
             <div className="col-lg-12">
@@ -35,22 +35,34 @@ export default async function BlogsArchivePage() {
               blogs.map((blog, idx) => (
                 <div className="col-lg-4 col-md-6 mb-5" key={blog._id || idx}>
                   <Link href={`/blogs/${blog.slug || "#"}`}>
-                    <div className="blog-card" style={{ cursor: "pointer", height: "100%" }}>
-                      <div className="blog-image relative">
+                    <div className="blog-card" style={{ cursor: "pointer", height: "100%", background: "#fff", borderRadius: "20px", overflow: "hidden" }}>
+                      <div className="locations-image relative">
                         <img
                           src={blog.imageURL || "/assets/blogs/1748253643_1.png"}
                           alt={blog.title}
                           style={{ objectFit: "cover", width: "100%", height: "300px" }}
                         />
+                        <div className="shape-bottom">
+                          <div className="shape-left-top">
+                            <svg className="w-11 h-11" fill="none" style={{ width: "1.0rem", height: "1.0rem" }} viewBox="0 0 11 11" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z" fill="#fff"></path>
+                            </svg>
+                          </div>
+                          <div className="shape-right-bottom">
+                            <svg className="w-11 h-11" fill="none" style={{ width: "1.2rem", height: "1.2rem" }} viewBox="0 0 11 11" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z" fill="#fff"></path>
+                            </svg>
+                          </div>
+                        </div>
                       </div>
-                      <div className="blog-content text-shape relative">
+                      <div className="blog-content text-shape relative" style={{ padding: "20px" }}>
                         <div className="blog-para">
-                          <p style={{ display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical", overflow: "hidden" }}>
+                          <p style={{ display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical", overflow: "hidden", fontSize: "20px", fontWeight: "600", color: "#000", fontFamily: "Montserrat, sans-serif" }}>
                             {blog.title}
                           </p>
                         </div>
-                        <div className="blog-date textright">
-                          <span className="font17 textright" style={{ display: "block" }}>
+                        <div className="blog-date textright" style={{ marginTop: "15px" }}>
+                          <span className="font17 textright" style={{ display: "block", color: "#575757" }}>
                             {(blog.publishDate || blog.createdAt)
                               ? new Date(blog.publishDate || blog.createdAt).toLocaleDateString(
                                   "en-US",
